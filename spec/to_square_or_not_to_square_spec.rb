0916371ffa167d9ebaebfb75bb619aa7f741d8e5
Albert <ayanit1@googlemail.com>
2017-02-10 16:03:02 +0000

    Added specifications for to square or not to square kata

---
+++ spec/to_square_or_not_to_square_spec.rb
@@ -0,0 +1,7 @@
+require 'to_square_or_not_to_square'
+
+describe SquareOrSquareRoot do
+  it 'squares or square roots integers' do
+    expect(subject.square_or_square_root([4, 3, 9, 7, 2, 1])).to match_array([2, 9, 3, 49, 4, 1])
+  end 
+end
