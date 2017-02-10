17d2023b659375d8614045c4c0576d92cacd732d
Albert <ayanit1@googlemail.com>
2017-02-10 18:30:01 +0000

    Corrected mistake in test

--- spec/fix_the_bugs_my_first_kata_spec.rb
+++ spec/fix_the_bugs_my_first_kata_spec.rb
@@ -2,6 +2,7 @@ require 'fix_the_bugs_my_first_kata'
 
 describe MyFirstKata do
   it 'returns values' do
-    expect(project.my_first_kata(3, 5)).to eq(3 % 5 + 5 % 3)
+    expect(subject.my_first_kata(3, 5)).to eq(3 % 5 + 5 % 3)
   end
 end
+git 
